af0bfbafb4c075a963b26bd9f0ee4537ad709732
WE-DA-pluggg <79959576+WE-DA-pluggg@users.noreply.github.com>
2022-05-30 09:07:34 +0900

    ㄷㄷ

--- myblog/src/main/java/com/sparta/myblog/controller/UserController.java
+++ myblog/src/main/java/com/sparta/myblog/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.sparta.myblog.controller;
 
 import com.sparta.myblog.domain.SignupRequestDto;
+import com.sparta.myblog.domain.UserInfoDto;
+import com.sparta.myblog.security.UserDetailsImpl;
 import com.sparta.myblog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.UUID;
 
@@ -32,6 +36,16 @@ public class UserController {
         }
         return "login";
     }
+
+    // 회원 관련 정보 받기
+    @PostMapping("/user/userinfo")
+    @ResponseBody
+    public UserInfoDto getUserInfo(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        String username = userDetails.getUser().getUsername();
+
+        return new UserInfoDto(username);
+    }
+
 
     // 회원 가입 페이지
     @GetMapping("/user/signup")
