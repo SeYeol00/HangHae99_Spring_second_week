b359f6d79e0b58fef09d4a72f2e3b4e153043f2b
WE-DA-pluggg <79959576+WE-DA-pluggg@users.noreply.github.com>
2022-06-01 09:08:28 +0900

    Update JwtAuthFilter.java

--- myblog/src/main/java/com/sparta/myblog/security/filter/JwtAuthFilter.java
+++ myblog/src/main/java/com/sparta/myblog/security/filter/JwtAuthFilter.java
@@ -41,7 +41,8 @@ public class JwtAuthFilter extends AbstractAuthenticationProcessingFilter {
         // JWT 값을 담아주는 변수 TokenPayload
         String tokenPayload = request.getHeader("Authorization");
         if (tokenPayload == null) {
-            response.sendRedirect("/user/loginView");
+            response.addHeader("error","로그인을 해주세요.");
+            response.sendRedirect("/");
             return null;
         }
 
