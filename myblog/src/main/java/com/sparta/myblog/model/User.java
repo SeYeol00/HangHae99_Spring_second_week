c25e090e0f0015c74a03c377d2dc4162e8fc019c
WE-DA-pluggg <79959576+WE-DA-pluggg@users.noreply.github.com>
2022-06-02 16:56:12 +0900

    안녕하세요

--- myblog/src/main/java/com/sparta/myblog/model/User.java
+++ myblog/src/main/java/com/sparta/myblog/model/User.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 @Getter // get 함수를 일괄적으로 만들어줍니다.
 @NoArgsConstructor // 기본 생성자를 만들어줍니다.
 @Entity // DB 테이블 역할을 합니다.
-@Table(name="User_info")
+@Table(name="User_info_myblog")
 public class User {
 
     // ID가 자동으로 생성 및 증가합니다.
